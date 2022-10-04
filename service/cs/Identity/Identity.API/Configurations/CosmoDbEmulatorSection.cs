12002459c3f78784545fa198c8ff225d43835a90
CharlieGitDB <CharlieGitDB@users.noreply.github.com>
2022-10-03 21:37:39 -0500

    delete emulator and localmssql section configuration files

--- service/cs/Identity/Identity.API/Configurations/CosmoDbEmulatorSection.cs
+++
@@ -1,8 +0,0 @@
-namespace Identity.API.Configurations;
-
-public class CosmoDbEmulatorSection
-{
-    public string EndPointUri { get; set; }
-    public string PrimaryKey { get; set; }
-    public string DbName { get; set; }
-}
